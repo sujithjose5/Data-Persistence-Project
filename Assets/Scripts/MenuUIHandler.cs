d0c04af26ab1a804a53de990268d38ecd1084103
Sujith Jose <31940692+sujithjose5@users.noreply.github.com>
2023-03-03 21:57:14 -0600

    - Added Exit button functionality
    - Exit button quits the game

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
 public class MenuUIHandler : MonoBehaviour
 {
     public Text ScoreText;
@@ -37,5 +41,15 @@ public class MenuUIHandler : MonoBehaviour
     public void UpdateHighScoreTextInMenu()
     {
         ScoreText.text = "Best Score:" +  MenuManager.Instance.highScoreName + "  Score:" + MenuManager.Instance.highScoreValue;
+    }
+
+    // Method for Exit button
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+    EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
